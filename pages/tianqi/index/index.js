34612bc41a7b33fdbe42a328ecc2a7a53bc2a570
shinepans <shinepans@live.com>
2018-08-10 14:48:55 +0800

    chose area

--- pages/tianqi/index/index.js
+++ pages/tianqi/index/index.js
@@ -5,12 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    laitude: '',
-    longitude: '',
-    speed: '',
-    accuracy: '',
-    city: '',
+    city: '选择城市',
+    display: 'chose_city',
     data: '',
+    index: '',
+    funcs: [
+      '南开区',
+      '武昌区',
+      '洪山区',
+      '青山区',
+      '咸安区'
+    ],
     index_icon: [
       'http://7xq5e8.com1.z0.glb.clouddn.com/beans/weather/kongtiao.png', 
       'http://7xq5e8.com1.z0.glb.clouddn.com/beans/weather/yundong.png', 
@@ -25,41 +30,36 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    let that = this
-    wx.getLocation({
-      type: 'wgs84',
-      success: function(res) {
-        let latitude = res.latitude
-        let longitude = res.longitude
-        let speed = res.speed
-        let accuracy = res.accuracy
-        let altitude = res.altitude
-        console.log(res)
+    const weather_city = wx.getStorageSync('weather_city') || ''
+    if (weather_city !== '') {
+      this.setData({city: weather_city, display: 'index'})
+      this.getData(weather_city)
+    }
+  },
+
+  choseNewCity: function (options) {
+    this.setData({display: 'chose_city'})
+  },
+
+  getData: function (city) {
+    const reqUrl = 'https://jisutqybmf.market.alicloudapi.com/weather/query?city=' + `${city || this.data.funcs[this.data.index]}`
+    const that = this
+    wx.request({
+      url: reqUrl,
+      header: {
+        "X-Ca-Timestamp": Date.now().toString(),
+        "gateway_channel": "https",
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": "APPCODE cdd16ea4905749b2ba2adeb6b9aea054"
+      },
+      success: function (tq) {
+        wx.setStorageSync('tianqi_data', tq.result)
+        console.log(tq.data)
+        console.log(tq.data.result.city)
         that.setData({
-          altitude: altitude,
-          latitude: latitude,
-          longitude: longitude,
-          speed: speed,
-          accuracy: accuracy
-        })
-        const reqUrl = 'https://jisutqybmf.market.alicloudapi.com/weather/query?location=' + `${latitude.toString()},${longitude.toString()}`
-        wx.request({
-          url: reqUrl,
-          header: {
-            "X-Ca-Timestamp": Date.now().toString(),
-            "gateway_channel": "https",
-            "Content-Type": "application/json; charset=utf-8",
-            "Authorization": "APPCODE cdd16ea4905749b2ba2adeb6b9aea054"
-          },
-          success: function(tq) {
-            wx.setStorageSync('tianqi_data', tq.result)
-            console.log(tq.data)
-            console.log(tq.data.result.city)
-            that.setData({
-              data: tq.data.result,
-              city: tq.data.result.city
-            })
-          }
+          display: 'index',
+          data: tq.data.result,
+          city: tq.data.result.city
         })
       }
     })
@@ -77,6 +77,14 @@ Page({
    */
   onShow: function() {
 
+  },
+
+  changeFunc: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
+    wx.setStorageSync('weather_city', this.data.funcs[e.detail.value])
+    this.getData(this.data.funcs[e.detail.value])
   },
 
   /**
